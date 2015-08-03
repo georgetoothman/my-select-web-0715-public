bf014c5795a6d0b31723dfe0484d54d9041d736e
George Toothman <george.toothman@gmail.com>
2015-08-03 09:59:46 -0400

    make my own select method

---
+++ lib/my_select.rb
@@ -0,0 +1,14 @@
+def my_select(array)
+  new_array = []
+  index = 0
+  while index < index
+    yield
+    new_array << array
+  end
+  new_array
+end
+
+# * What is the return value of yielding a character to a block?
+# * How can you ensure your select method can respond 
+# * appropriately to being passed an argument of an empty collection?
+# if it returns true shovel that thing into the array
